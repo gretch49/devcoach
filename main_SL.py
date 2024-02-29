eeccd331125623b7210abc69ffcd80d4805ebeef
gretch49 <160289596+gretch49@users.noreply.github.com>
2024-02-28 19:30:46 -0500

    Update main_SL.py

--- main_SL.py
+++ main_SL.py
@@ -7,6 +7,7 @@ from langchain.prompts import ChatPromptTemplate
 # Header
 st.header("dev Coach")
 st.subheader("A virtual tutor designed to assist students in learning coding through retrieval-augmented generation.", divider='rainbow')
+st.caption('This is a string that explains something above.', divider=True)
 
 MD_CHROMA_PATH = "data/chroma_md"  # Path for Markdown files
 TXT_CHROMA_PATH = "data/chroma_txt"  # Path for text files
