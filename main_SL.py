41834b9edef7b1323ce9a0cd7b31e75ed661a965
gretch49 <160289596+gretch49@users.noreply.github.com>
2024-03-04 12:27:26 -0500

    Update main_SL.py

--- main_SL.py
+++ main_SL.py
@@ -39,7 +39,7 @@ def main():
     if password != "" and password != "supersecret":
         st.sidebar.markdown(':red[*Incorrect password*]')
     elif password == "supersecret":
-        openai_api_key = st.secrets["openai_api_key"]
+        openai_api_key = st.secrets["OPENAI_API_KEY"]
 
     if not openai_api_key and password != "supersecret":
         st.info("Please provide the OpenAI API key or Gretchen's password to continue.")
